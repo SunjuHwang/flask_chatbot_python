3388556f2f185d076be2f21fa2f135e27c679a65
SunjuHwang <tjswn203@gmail.com>
2018-08-30 01:40:24 +0000

    keyboard api set

--- .~c9_invoke_Qb2zZ.py
+++ .~c9_invoke_Qb2zZ.py
@@ -1,20 +1,6 @@
-# 파이썬 챗봇 만들기!!
-
-### 카카오톡 플러스친구 관리자 센터 접속
-
-- 플러스 친구 생성 후 공개 설정(공개 안되면 검색 불가)
-- 스마트 채팅 API형 사용
-
-### C9 개발
-
-- 우측 상단의 톱니바퀴에 들어가서 python 3로 설정 변경
-- `sudo pip3 install flask` 플라스크 설치
-
-### keyboard
-
-``` python
 from flask import Flask
 import os
+#json으로 바꾸기 위해 라이브러리 추가 
 import json
 
 app = Flask(__name__)
@@ -25,14 +11,15 @@ def hello():
 
 @app.route('/keyboard')
 def keyboard():
+    
+    #keyboard 딕셔너리 생성
     keyboard = {
                 "type" : "buttons",
                 "buttons" : ["메뉴", "로또", "고양이", "영화"]
                 }
+                
+    #딕셔너리를 json으로 바꿔서 리턴 해주기
     json_keyboard = json.dumps(keyboard)            
     return json_keyboard
                     
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-
-```
-
